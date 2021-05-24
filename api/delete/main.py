d5eb942a4d56637e5f8eadd564b15b7313a1c396
hjpotter92 <github@hjpotter92.email>
2021-05-24 15:49:34 +0530

    Drop registered event when deletion lambda exits.

--- api/delete/main.py
+++ api/delete/main.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from os import getenv
 
 import boto3
@@ -12,10 +13,14 @@ class Config:
 def delete_id(_id: str):
     db = boto3.resource("dynamodb")
     s3 = boto3.resource("s3")
+    events = boto3.client("events")
     table = db.Table(Config.DYNAMODB_TABLE)
     table.delete_item(Key={"id": _id})
     s3_object = s3.Object(Config.S3_BUCKET, _id)
     s3_object.delete()
+    rule_name = f"pastely-delete-{_id}"
+    events.remove_targets(Rule=rule_name, Ids=["1"])
+    events.delete_rule(Name=rule_name)
     print("Deleted item.")
 
 
